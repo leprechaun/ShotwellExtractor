8f23a3dc9933dd3c4ffd2bc39abaf9a2b0337daf
Lawrence MacGuire <leprechaun@gmail.com>
2013-11-05 21:51:02 +0800

    First version of shotwell extractor

---
+++ extract.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+"""Usage:
+  test.py [--database=<database>] [--include-tags=<tags>] [--exclude-tags=<tags>] [--from-date=<date>] [--export-path=<path>] [--thumbnail-path=<path>]
+"""
+from docopt import docopt
+
+import sqlite3
+import os.path
+import json
+import shutil
+
+import sqlalchemy
+#from sqlalchemy import create_engine
+#from sqlalchemy.orm import sessionmaker
+
+from shotwellextractor.entities import *
+
+arguments = docopt(__doc__, version='0.1.1rc')
+if arguments['--database'] is not None:
+    path = os.path.expanduser(arguments['--database'])
+else:
+    path = os.path.expanduser("~/.local/share/shotwell/data/photo.db")
+
+
+engine = create_engine('sqlite:///' + path)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+exclude = []
+if arguments['--exclude-tags'] is not None:
+    exclude = arguments['--exclude-tags'].split(',')
+
+include = []
+if arguments['--include-tags'] is not None:
+    include = arguments['--include-tags'].split(',')
+
+from_date = None
+if arguments['--from-date'] is not None:
+    from_date = arguments['--from-date']
+
+export_path = "output/"
+if arguments['--export-path'] is not None:
+    export_path = arguments['--export-path']
+
+if arguments['--thumbnail-path'] is not None:
+    thumbnail_path = os.path.expanduser(arguments['--thumbnail-path'])
+    if not os.path.isdir(thumbnail_path):
+        exit()
+
+shutil.rmtree(export_path)
+os.mkdir(export_path)
+os.mkdir(export_path + "/pictures")
+os.mkdir(export_path + "/tags")
+os.mkdir(export_path + "/thumbnails")
+
+def remove_ids_from_list(l, ids):
+    [l.remove(i) for i in ids]
+
+def chunks(l, n):
+    """ Yield successive n-sized chunks from l.
+    """
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+print("From Date:", from_date)
+print("Exclude:", exclude)
+print("Include:", include)
+
+if include == []:
+    tags = session.query(Tag).all()
+else:
+    tags = session.query(Tag).filter(Tag.name.in_(include))
+
+if exclude is not []:
+    exlusions = []
+    exclude_tags = session.query(Tag).filter(Tag.name.in_(exclude))
+    exclude_photos = []
+    for ex_tag in exclude_tags:
+        exclude_photos.extend(ex_tag.photo_list)
+
+    exclude_photos = list(set(exclude_photos))
+
+photo_list = []
+for tag in tags:
+    photo_list.extend(tag.photo_list)
+
+photo_list = list(set(photo_list))
+print("Unfiltered pictures:", str(len(photo_list)))
+#[photo_list.remove(ex) for ex in exclude_photos]
+remove_ids_from_list(photo_list, exclude_photos)
+print("Filtered pictures:", str(len(photo_list)))
+
+def dump_json(obj, dest):
+    js = json.dumps(obj)
+    with open(dest, 'w+') as f:
+        f.write(js)
+
+def as_dict(obj, attr):
+    attr = attr.split(",")
+    return {c: getattr(obj, c) for c in attr}
+
+all_pictures = []
+
+
+tag_picture_hash = {t.name: t.photo_list for t in tags}
+tagsjs = {}
+
+for chunk in chunks(photo_list, 100):
+        photo_chunk = session.query(Photo).filter(Photo.id.in_(chunk))
+        for p in photo_chunk:
+            p.tags = []
+            for tag in tag_picture_hash:
+
+                if p.id in tag_picture_hash[tag]:
+                    p.tags.append(tag)
+                    if tag not in tagsjs:
+                        tago = {'name': tag, 'thumbnail': p.thumbnail, 'picture_count': 1, 'pictures': [p]}
+                        tagsjs[tag] = tago
+                    else:
+                        tagsjs[tag]['pictures'].append(p)
+                        tagsjs[tag]['picture_count'] = tagsjs[tag]['picture_count'] + 1
+
+            pdict = as_dict(p, "id,path,thumbnail,exposure_time,orientation,tags")
+            pdict['size'] = {'height': p.height, 'width': p.width}
+            dump_json(pdict, export_path + "/pictures/" + str(p.id) + ".json")
+
+            all_pictures.append(as_dict(p, "id,thumbnail,path"))
+            shutil.copy(thumbnail_path + "/" + p.thumbnail, export_path + "/thumbnails/" + p.thumbnail)
+
+tags_file = [{'name': t, 'picture_count': tagsjs[t]['picture_count'], 'thumbnail': tagsjs[t]['thumbnail']} for t in tagsjs]
+
+for t in tagsjs:
+    to = {'name': t, 'pictures': [], 'thumbnail': tagsjs[t]['thumbnail']}
+    for p in tagsjs[t]['pictures']:
+        to['pictures'].append(as_dict(p, "id,thumbnail,title,path"))
+    dump_json(to, export_path + "/tags/" + t + ".json")
+
+dump_json(all_pictures, export_path + "/pictures/all-pictures.json")
+dump_json(tags_file, export_path + "/tags/all-tags.json")
---
+++ shotwellextractor/entities.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+from sqlalchemy import Table, MetaData, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import mapper
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import event
+
+import stagger
+
+import os.path
+
+import urllib.parse
+
+Base = declarative_base()
+
+class Photo(Base):
+    __tablename__ = "phototable"
+
+    id = Column(Integer, primary_key=True)
+    filename = Column(String)
+    width = Column(Integer)
+    height = Column(Integer)
+    filesize = Column(Integer)
+    exposure_time = Column(Integer)
+    orientation = Column(Integer)
+    original_orientation = Column(Integer)
+    import_id = Column(Integer)
+    event_id = Column(Integer)
+    transformations = Column(String)
+    md5 = Column(String)
+    thumbnail_md5 = Column(String)
+    exif_md5 = Column(String)
+    time_created = Column(Integer)
+    flags = Column(Integer)
+    rating = Column(Integer)
+    file_format = Column(Integer)
+    title = Column(String)
+    backlinks = Column(String)
+    time_reimported = Column(Integer)
+    editable_id = Column(Integer)
+    metadata_dirty = Column(Integer)
+    developer = Column(String)
+    develop_shotwell_id = Column(Integer)
+    develop_camera_id = Column(Integer)
+    develop_embedded_id = Column(Integer)
+    comment = Column(String)
+
+    @property
+    def path(self):
+        return self.filename.lstrip("/home/leprechaun/Pictures")
+
+    @property
+    def thumbnail(self):
+        id = str(hex(self.id)).lstrip("0x")
+        # thumb0000000000000a0c
+        return "thumb" + id.rjust(16, "0") + ".jpg"
+
+    def __repr__(self):
+        return "<Photo(#%s '%s')>" % (self.id, self.filename)
+
+class Event(Base):
+    __tablename__ = "eventtable"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    primary_photo_id = Column(Integer)
+    time_created = Column(Integer)
+    primary_source_id = Column(Integer)
+    comment = Column(String)
+
+class Tag(Base):
+    __tablename__ = "tagtable"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    photo_id_list = Column(String)
+    time_created = Column(Integer)
+
+    @property
+    def photo_list(self):
+        if self.photo_id_list is None:
+            return []
+
+        l = self.photo_id_list.split(",")
+        # last is always empty
+        l.pop()
+
+        # strip "thumb" out and we've got hex left
+        l = [id[5:] for id in l]
+        l = [int(id.lstrip("0"),16) for id in l]
+        return l
+
+    @photo_list.setter
+    def photo_list(self, photo_list):
+        self.photo_id_list = ",".join(photo_list)
+
+    def __repr__(self):
+        return "<Tag(#%s '%s')>" % (self.id, self.name)
